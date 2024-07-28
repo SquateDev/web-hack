23a27b782479f973359896d3a62234434d33782f
SquateDev <sosantyan198@gmail.com>
2024-07-28 11:32:28 +0300

    Update webhack.py

--- webhack.py
+++ webhack.py
@@ -2,10 +2,9 @@ import argparse
 import requests, json
 import sys
 from sys import argv
-import os
+import os, Fore,
 
 
 api = "https://www.ufamama.ru/Auth/ActivationByPhoneForm?phone=%2B79016929013"
 data = requests.get(api).json();
-print("Socket ", data['alert-warning'])
-
+print(Fore.RED+"Done Sms"+Fore.WHITE+"...");
