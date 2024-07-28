8c7b949603c814f15e5196f2c238be3bbabbf0e5
SquateDev <sosantyan198@gmail.com>
2024-07-28 11:30:09 +0300

    Update webhack.py

--- webhack.py
+++ webhack.py
@@ -7,5 +7,5 @@ import os
 
 api = "https://www.ufamama.ru/Auth/ActivationByPhoneForm?phone=%2B79016929013"
 data = requests.get(api).json();
-print("Socket ", data['query'])
+print("Socket ", data['alert alert-warning'])
 
