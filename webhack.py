fd07a41769a1c6e7226543920fd8b09d7d6ba84a
SquateDev <sosantyan198@gmail.com>
2024-07-28 11:35:53 +0300

    Update webhack.py

--- webhack.py
+++ webhack.py
@@ -7,5 +7,5 @@ from colorama import Fore, Style
 
 
 api = "https://www.ufamama.ru/Auth/ActivationByPhoneForm?phone=%2B79016929013"
-data = requests.get(api).json();
+data = requests.get(api);
 print(Fore.RED+"Done Sms"+Fore.WHITE+"...");
